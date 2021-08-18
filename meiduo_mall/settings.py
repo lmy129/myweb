651fb18b21cdb0fcdb00d8e204a295d02b5f4107
liu <fa1292689898@gmail.com>
2021-08-18 18:07:54 +0800

    设置数据库和安装pymysql

--- meiduo_mall/settings.py
+++ meiduo_mall/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'meiduo',
+        'PORT': '3306',
+        'HOST': '127.0.0.1',
+        'USER': 'root',
+        'PASSWORD': 'liumengyan',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
