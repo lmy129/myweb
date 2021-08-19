7dd9a55702b54f7859dd7c2958f9ea6a2ab02165
liu <fa1292689898@gmail.com>
2021-08-19 16:33:03 +0800

    创建用户模型

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    #定义用户模型
+    mobile = models.CharField(verbose_name="手机号",max_length=11,unique=True)
+
+    class Meta:
+        verbose_name = "用户管理"
+        verbose_name_plural = verbose_name
