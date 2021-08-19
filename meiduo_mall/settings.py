71e82adfef61c9adf0f1e3cef26c2f9154e5e764
liu <fa1292689898@gmail.com>
2021-08-19 11:29:19 +0800

    更新radis设置

--- meiduo_mall/settings.py
+++ meiduo_mall/settings.py
@@ -129,6 +129,28 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#django-redis配置
+CACHES = {
+    "default":{
+        #默认
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":'redis://127.0.0.1:6379/0',
+        "OPTIONS":{
+            "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    },
+    "session":{
+        #默认
+        "BACKEND":"django_redis.cache.RedisCache",
+        "LOCATION":'redis://127.0.0.1:6379/1',
+        "OPTIONS":{
+            "CLIENT_CLASS":"django_redis.client.DefaultClient",
+        }
+    },
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
 
 #日志设置
 LOGGING = {
